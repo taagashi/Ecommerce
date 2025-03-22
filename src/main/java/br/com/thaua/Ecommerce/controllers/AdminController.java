42cd0bfba747ba6057e30390e07d80295d1b7084
Thauã Gabriel Sousa e Silva <tagashi.dev@gmail.com>
2025-03-22 11:28:30 -0300

    refactor: adicionar autenticação via swagger

--- src/main/java/br/com/thaua/Ecommerce/controllers/AdminController.java
+++ src/main/java/br/com/thaua/Ecommerce/controllers/AdminController.java
@@ -2,6 +2,8 @@ package br.com.thaua.Ecommerce.controllers;
 
 import br.com.thaua.Ecommerce.dto.ClienteResponse;
 import br.com.thaua.Ecommerce.services.AdminService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +18,7 @@ import java.util.List;
 public class AdminController {
     private final AdminService adminService;
 
+    @Operation(summary = "Listar clientes", description = "Lista todos os clientes", security = @SecurityRequirement(name = "bearerAuth"))
     @GetMapping
     public ResponseEntity<List<ClienteResponse>> listarClientes() {
         return ResponseEntity.ok(adminService.listarClientes());
