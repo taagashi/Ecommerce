90f0ee0b8a4bb8651e0db65b32a7324b8d4e9e26
Thauã Gabriel Sousa e Silva <tagashi.dev@gmail.com>
2025-04-03 11:16:53 -0300

    feat: adicionar handler de exceptions

---
+++ src/main/java/br/com/thaua/Ecommerce/controllers/handler/ExceptionHandlerClass.java
@@ -0,0 +1,9 @@
+package br.com.thaua.Ecommerce.controllers.handler;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionHandlerClass {
+
+}
