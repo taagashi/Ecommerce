ec33220bbf2a73fd58002783159b8d39b019fce1
Thauã Gabriel Sousa e Silva <tagashi.dev@gmail.com>
2025-04-14 17:10:16 -0300

    feat: adicionar cache para controller de fornecedor

--- src/main/java/br/com/thaua/Ecommerce/controllers/FornecedorController.java
+++ src/main/java/br/com/thaua/Ecommerce/controllers/FornecedorController.java
@@ -11,12 +11,17 @@ import br.com.thaua.Ecommerce.services.FornecedorService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import br.com.thaua.Ecommerce.controllers.handler.ConstructorErrors;
 import java.util.List;
 
+@Slf4j
 @SecurityRequirement(name = "bearerAuth")
 @RequiredArgsConstructor
 @RestController
@@ -25,6 +30,7 @@ public class FornecedorController {
     private final FornecedorService fornecedorService;
 
     //    PATCH /api/v1/funcionarios/cnpj-telefone/update - atualizar cnpj e telfone [ROLE: FUNCIONARIOS]
+    @CachePut(value = "forneceedores", key = "T(org.springframework.security.core.context.SecurityContextHolder).getContext().getAuthentication().getPrincipal().getUsername()")
     @Operation(summary = "atualizar CNPJ e telefone", description = "fornecedor pode atualizar dados como cnpj e telefone")
     @PatchMapping("/cnpj-telefone/update")
     public ResponseEntity<FornecedorResponse> atualizarCNPJeTelefone(@RequestBody FornecedorCNPJTelefoneRequest fornecedorCNPJTelefoneRequest) {
@@ -32,6 +38,7 @@ public class FornecedorController {
     }
 
 //    POST /api/V1/funcionarios/produtos/register - Cadastrar novo produto [ROLE FUNCIONARIO]
+    @CacheEvict(value = "produtos", allEntries = true)
     @Operation(summary = "cadastrar produto", description = "fornecedor só pode cadastrar produtos se tiver com CNPJ, telefone e endereço cadastrados")
     @PostMapping("/produtos/register")
     public ResponseEntity<ProdutoResponse> cadastrarProduto(@RequestBody ProdutoRequest produtoRequest) {
@@ -39,6 +46,7 @@ public class FornecedorController {
     }
 
 //    GET /api/v1/funcionarios/produtos/list - Listar produtos (com paginação e filtros) [ROLE: FUNCIONARIO]
+    @Cacheable(value = "produtos", key = "T(org.springframework.security.core.context.SecurityContextHolder).getContext().getAuthentication().getPrincipal().getUsername() + '_' + #pageable.getPageNumber() + '_' + #pageable.getPageSize()")
     @Operation(summary = "listar produtos", description = "Fornecedor faz a listagem de todos os produtos que ele cadastrou")
     @GetMapping("produtos/list")
     public ResponseEntity<Pagina<ProdutoResponse>> exibirProdutos(Pageable pageable) {
@@ -46,6 +54,7 @@ public class FornecedorController {
     }
 
 //    - `GET /api/v1/funcionarios/produtos/{produtoId}/list - Buscar produto por ID [ROLE: FUNCIONARIO]
+    @Cacheable("produtos")
     @Operation(summary = "exibir um produto do fornecedor", description = "Fornecedor pode olhar um produto especifico seu")
     @GetMapping("/produtos/{produtoId}")
     public ResponseEntity<ProdutoResponse> buscarProduto(@PathVariable Long produtoId) {
@@ -54,6 +63,7 @@ public class FornecedorController {
 
 //    PRECISO ARRUMAR ESSA LOGICA AQUI QUE TA LA NO SERVICE
 //    PUT /api/v1/funcionarios/produtos/{id}/update - Atualizar produto
+    @CacheEvict(value = "produtos", allEntries = true)
     @Operation(summary = "atualizar informacoes de produto", description = "fornecedor pode atualizar as informacoes de um produto especifico")
     @PutMapping("/produtos/{produtoId}/update")
     public ResponseEntity<ProdutoResponse> atualizarProduto(@PathVariable Long produtoId, @RequestBody ProdutoRequest produtoRequest) {
@@ -68,6 +78,7 @@ public class FornecedorController {
     }
 
 //    PATCH /api/v1/funcionarios/produtos/{id}/estoque/update - Atualizar estoque do produto [ROLE: FUNCIONARIO]
+    @CacheEvict(value = "produtos", allEntries = true)
     @Operation(summary = "atualizar estoque", description = "fornecedor pode atualizar o estoque de um produto especifico")
     @PatchMapping("/produtos/{id}/estoque/update")
     public ResponseEntity<ProdutoResponse> atualizarEstoqueProduto(@PathVariable Long id, @RequestBody ProdutoNovoEstoqueRequest produtoNovoEstoqueRequest) {
@@ -75,6 +86,7 @@ public class FornecedorController {
     }
 
 //    DELETE /api/v1/funcionarios/produtos/{produtoId}/delete - Remover produto [ROLE FUNCIONARIO]
+    @CacheEvict(value = "produtos", allEntries = true)
     @Operation(summary = "remover produto", description = "fornecedor pode remover um de seus produtos")
     @DeleteMapping("/produtos/{produtoId}/delete")
     public ResponseEntity<String> removerProduto(@PathVariable Long produtoId) {
