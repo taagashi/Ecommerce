31131ce36820c967aef5fef1816627825c8121fd
Thauã Gabriel Sousa e Silva <tagashi.dev@gmail.com>
2025-04-14 18:41:43 -0300

    feat: adicionar cache para controller de produto

--- src/main/java/br/com/thaua/Ecommerce/controllers/ProdutoController.java
+++ src/main/java/br/com/thaua/Ecommerce/controllers/ProdutoController.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,12 +23,14 @@ import java.math.BigDecimal;
 public class ProdutoController {
     private final ProdutoService produtoService;
 
+    @Cacheable("categoria-Produto")
     @Operation(summary = "Lista todas as categorias de um determinado produto", description = "faz a listagem de categorias de um produto atraves de seu id")
     @GetMapping("/{produtoId}/categorias/list")
     public ResponseEntity<ProdutoCategoriaResponse> exibirCategoriasDeProduto(@PathVariable Long produtoId) {
         return ResponseEntity.ok(produtoService.exibirCategoriasDeProduto(produtoId, ConstructorErrors.returnMapErrors()));
     }
 
+    @Cacheable("produtos")
     @Operation(summary = "Lista todos os produtos com filtrando os precos", description = "lista todos os produtos usando um filtro de precos para o usuario poder ver qual eh o produto mais barato e afins")
     @GetMapping("/list")
     public ResponseEntity<Pagina<ProdutoResponse>> exibirProdutos(
