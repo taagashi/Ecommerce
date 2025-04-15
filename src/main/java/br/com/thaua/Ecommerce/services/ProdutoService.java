0f5d4e602482e84fc808dfba4fff1cdfee1fda4d
Thauã Gabriel Sousa e Silva <tagashi.dev@gmail.com>
2025-04-14 22:01:29 -0300

    feat: adicionar logs para camada de servico produto

--- src/main/java/br/com/thaua/Ecommerce/services/ProdutoService.java
+++ src/main/java/br/com/thaua/Ecommerce/services/ProdutoService.java
@@ -11,6 +11,7 @@ import br.com.thaua.Ecommerce.repositories.ProdutoRepository;
 import br.com.thaua.Ecommerce.repositories.specifications.ProdutoSpecifications;
 import br.com.thaua.Ecommerce.services.validators.ValidationService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -19,6 +20,7 @@ import java.math.BigDecimal;
 import java.util.Map;
 import java.util.Optional;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class ProdutoService {
@@ -35,11 +37,15 @@ public class ProdutoService {
         validationService.validarExistenciaEntidade(produtoEntity.orElse(null), errors);
         validationService.analisarException("Houve um erro ao tentar exibir categorias de um produto", ProdutoNotFoundException.class, errors);
 
+
+        log.info("EXECUTANDO SERVICE-PRODUTO EXIBIR CATEGORIAS PRODUTOS");
         return produtoMapper.toProdutoCategoriaResponse(produtoEntity.get());
     }
 
     @Cacheable("produtos")
     public Pagina<ProdutoResponse> exibirProdutos(Pageable pageable, BigDecimal min, BigDecimal max) {
+
+        log.info("EXECUTANDO SERVICE-PRODUTO EXIBIR PRODUTOS");
         return paginaMapper.toPagina(produtoSpecifications.buscarComFiltros(min, max, pageable).map(produtoMapper::produtoToResponse));
     }
 }
