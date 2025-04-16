22f4c822388469844c64c995beedee04996bb086
Thauã Gabriel Sousa e Silva <tagashi.dev@gmail.com>
2025-04-15 21:40:49 -0300

    feat: adicionar envio de email para redefinir senha

--- src/main/java/br/com/thaua/Ecommerce/services/EmailMessageService.java
+++ src/main/java/br/com/thaua/Ecommerce/services/EmailMessageService.java
@@ -6,12 +6,25 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
+
 @RequiredArgsConstructor
 @Service
 public class EmailMessageService {
     private final JavaMailSender mailSender;
     @Value("${spring.mail.username}")
     private String remetente;
+
+    public void registroDeUsuario(String nome, String destinatario) {
+        enviarEmails("REGISTRO ECOMMERCE", "Parabéns " + nome + ", você acaba de se registrar no nosso Ecommerce :)", destinatario);
+    }
+
+    public void redefinirSenha(String nome, String destinatario) {
+        Random random = new Random();
+        int codigo = 100000 + random.nextInt(999999);
+
+        enviarEmails("REDEFINIR SENHA", nome + " seu código de verificação para mudança de senha é: " + codigo, destinatario);
+    }
 
     private void enviarEmails(String assunto, String texto, String destinatario) {
         SimpleMailMessage message = new SimpleMailMessage();
@@ -21,9 +34,5 @@ public class EmailMessageService {
         message.setFrom(remetente);
 
         mailSender.send(message);
-    }
-
-    public void registroDeUsuario(String nome, String destinatario) {
-        enviarEmails("Registro Ecommerce", "Parabéns " + nome + ", você acaba de se registrar no nosso Ecommerce :)", destinatario);
     }
 }
