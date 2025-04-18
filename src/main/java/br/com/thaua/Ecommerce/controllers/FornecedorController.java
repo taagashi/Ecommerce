94640bbf2a3dcab62b033e12abf4ef0eda6c025b
Thauã Gabriel Sousa e Silva <tagashi.dev@gmail.com>
2025-04-18 10:03:40 -0300

    feat: novo status para pedido

--- src/main/java/br/com/thaua/Ecommerce/controllers/FornecedorController.java
+++ src/main/java/br/com/thaua/Ecommerce/controllers/FornecedorController.java
@@ -97,10 +97,10 @@ public class FornecedorController {
         return ResponseEntity.ok(fornecedorService.removerProdutoDeCategoria(categoriaId, produtoId, ConstructorErrors.returnMapErrors()));
     }
 
-    @Operation(summary = "enviar pedido", description = "fornecedor acessa pedidos recebidos que estao como pagos para envialos para o cliente")
-    @PatchMapping("/produtos/{produtoId}/enviar")
-    public ResponseEntity<String> enviarProduto(@PathVariable Long produtoId) {
-        log.info("ENVIAR PEDIDO");
-        return ResponseEntity.ok(fornecedorService.enviarProduto(produtoId, ConstructorErrors.returnMapErrors()));
-    }
+//    @Operation(summary = "enviar pedido", description = "fornecedor acessa pedidos recebidos que estao como pagos para envialos para o cliente")
+//    @PatchMapping("/produtos/{produtoId}/enviar")
+//    public ResponseEntity<String> enviarProduto(@PathVariable Long produtoId) {
+//        log.info("ENVIAR PEDIDO");
+//        return ResponseEntity.ok(fornecedorService.enviarProduto(produtoId, ConstructorErrors.returnMapErrors()));
+//    }
 }
--- src/main/java/br/com/thaua/Ecommerce/domain/enums/StatusPedido.java
+++ src/main/java/br/com/thaua/Ecommerce/domain/enums/StatusPedido.java
@@ -2,14 +2,16 @@ package br.com.thaua.Ecommerce.domain.enums;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
-@AllArgsConstructor
 @Getter
+@RequiredArgsConstructor
 public enum StatusPedido {
     CANCELADO("Pedido foi cancelado"),
+    AGUARDANDO_PAGAMENTO("Aguardando pagamento do pedido"),
     PAGO("Pedido foi pago com sucesso"),
-    ENVIADO("Pedido foi enviado para o seu endereco"),
-    AGUARDANDO_PAGAMENTO("Aguardando pagamento do pedido");
+    PAGO_PROCESSANDO("Pedido está sendo processado"),
+    ENVIADO("Pedido foi enviado para o seu endereco");
 
     private final String message;
 }
