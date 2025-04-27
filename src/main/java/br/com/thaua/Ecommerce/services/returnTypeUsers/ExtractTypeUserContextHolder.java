baa6fb3f07ba5a930904ccb69a02343d414ca5cd
Thauã Gabriel Sousa e Silva <tagashi.dev@gmail.com>
2025-04-27 11:55:33 -0300

    feat: adicionar logs para extract type user contextHolder

--- src/main/java/br/com/thaua/Ecommerce/services/returnTypeUsers/ExtractTypeUserContextHolder.java
+++ src/main/java/br/com/thaua/Ecommerce/services/returnTypeUsers/ExtractTypeUserContextHolder.java
@@ -2,15 +2,19 @@ package br.com.thaua.Ecommerce.services.returnTypeUsers;
 
 import br.com.thaua.Ecommerce.domain.entity.UsersEntity;
 import br.com.thaua.Ecommerce.userDetails.MyUserDetails;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+@Slf4j
 public class ExtractTypeUserContextHolder {
     public static UsersEntity extractUser() {
+        log.info("EXTRACT TYPE USER CONTEXT HOLDER - EXTRACT USER");
         MyUserDetails myUserDetails = extractMyUserDetails();
         return myUserDetails.getUser();
     }
 
     private static MyUserDetails extractMyUserDetails() {
+        log.info("EXTRACT TYPE USER CONTEXT HOLDER - EXTRACT MY USER DETAILS");
         return (MyUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
 }
