21223c8b5c1c7d23a50cdafeddcf521614d35faa
Thauã Gabriel Sousa e Silva <tagashi.dev@gmail.com>
2025-05-03 10:54:37 -0300

    refactor: melhorar legibilidade de testes para controller endereco

--- src/test/java/br/com/thaua/Ecommerce/controllers/endereco/EnderecoControllerTest.java
+++ src/test/java/br/com/thaua/Ecommerce/controllers/endereco/EnderecoControllerTest.java
@@ -43,11 +43,17 @@ public class EnderecoControllerTest {
 
     private ObjectMapper objectMapper;
 
+    private Map<String, String> errors;
+
+    private Map<String, String> emptyMap;
+
     @BeforeEach
     public void setUp() {
         mockMvc = MockMvcBuilders.standaloneSetup(enderecoController)
                 .setControllerAdvice(new ExceptionHandlerClass())
                 .build();
+        errors = ConstructorErrors.returnMapErrors();
+        emptyMap = ConstructorErrors.returnMapErrors();
         objectMapper = new ObjectMapper();
     }
 
@@ -57,9 +63,8 @@ public class EnderecoControllerTest {
         EnderecoRequest enderecoRequest = ControllersFixture.createEnderecoRequest("Rua Mariano de Brito", "79", "Centro", "Cidade", "RN", "48323341");
         String enderecoRequestJson = objectMapper.writeValueAsString(enderecoRequest);
         EnderecoResponse enderecoResponse = ControllersFixture.createEnderecoResponse(1L, "taagashi", enderecoRequest.getRua(), enderecoRequest.getNumero(), enderecoRequest.getBairro(), enderecoRequest.getCidade(), enderecoRequest.getEstado(), enderecoRequest.getCep());
-        Map<String, String> errors = ConstructorErrors.returnMapErrors();
 
-        when(usersService.cadastrarEndereco(eq(enderecoRequest), eq(errors))).thenReturn(enderecoResponse);
+        when(usersService.cadastrarEndereco(eq(enderecoRequest), eq(emptyMap))).thenReturn(enderecoResponse);
 
         mockMvc.perform(post("/api/v1/users/enderecos/register")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -74,7 +79,7 @@ public class EnderecoControllerTest {
                 .andExpect(jsonPath("$.estado").value(enderecoResponse.getEstado()))
                 .andExpect(jsonPath("$.cep").value(enderecoResponse.getCep()));
 
-        verify(usersService, times(1)).cadastrarEndereco(enderecoRequest, errors);
+        verify(usersService, times(1)).cadastrarEndereco(eq(enderecoRequest), eq(emptyMap));
     }
 
     @DisplayName("Deve retornar com erro CadastroEnderecoException ao tentar cadastrar um endereço")
@@ -82,11 +87,13 @@ public class EnderecoControllerTest {
     public void testCadastrarEnderecoError() throws Exception{
         EnderecoRequest enderecoRequest = ControllersFixture.createEnderecoRequest("Rua Mariano de Brito", "79", "Centro", "Cidade", "Estado errado", "48323341");
         String errorMessage = ControllersFixture.createErrorMessage("usuario, houve um erro no cadastrado do endereco");
-        Map<String, String> errors = ConstructorErrors.returnMapErrors();
-        String enderecoRequestJson = objectMapper.writeValueAsString(enderecoRequest);
-        CadastroEnderecoException cadastroEnderecoException = new CadastroEnderecoException(errorMessage, Map.of("Endereço", "Endereço já cadastrado", "Estado", "Sigla de estado inválido"));
+        errors.put("Endereço", "Endereço já cadastrado");
+        errors.put("Estado", "Sigla de estado inválido");
 
-        when(usersService.cadastrarEndereco(eq(enderecoRequest), eq(errors))).thenThrow(cadastroEnderecoException);
+        String enderecoRequestJson = objectMapper.writeValueAsString(enderecoRequest);
+        CadastroEnderecoException cadastroEnderecoException = new CadastroEnderecoException(errorMessage, errors);
+
+        when(usersService.cadastrarEndereco(eq(enderecoRequest), eq(emptyMap))).thenThrow(cadastroEnderecoException);
 
         mockMvc.perform(post("/api/v1/users/enderecos/register")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -95,10 +102,10 @@ public class EnderecoControllerTest {
                 .andExpect(jsonPath("$.code").value(HttpStatus.BAD_REQUEST.value()))
                 .andExpect(jsonPath("$.message").value(errorMessage))
                 .andExpect(jsonPath("$.fieldsErrors").isMap())
-                .andExpect(jsonPath("$.fieldsErrors['Endereço']").value("Endereço já cadastrado"))
-                .andExpect(jsonPath("$.fieldsErrors['Estado']").value("Sigla de estado inválido"));
+                .andExpect(jsonPath("$.fieldsErrors['Endereço']").value(errors.get("Endereço")))
+                .andExpect(jsonPath("$.fieldsErrors['Estado']").value(errors.get("Estado")));
 
-        verify(usersService, times(1)).cadastrarEndereco(enderecoRequest, errors);
+        verify(usersService, times(1)).cadastrarEndereco(eq(enderecoRequest), eq(emptyMap));
     }
 
     @DisplayName("Deve retornar com sucesso um endereço exibido")
@@ -121,37 +128,36 @@ public class EnderecoControllerTest {
                 .andExpect(jsonPath("$.estado").value(enderecoResponse.getEstado()));
 
         verify(usersService, times(1))
-                .exibirEndereco(errors);
+                .exibirEndereco(eq(errors));
     }
 
     @DisplayName("Deve remover com sucesso um endereço")
     @Test
     public void testRemoverEnderecoSucesso() throws Exception {
-        Map<String, String> errors = ConstructorErrors.returnMapErrors();
 
         mockMvc.perform(delete("/api/v1/users/enderecos/delete"))
                 .andExpect(status().isOk());
 
-        verify(usersService, times(1)).deletarEndereco(errors);
+        verify(usersService, times(1)).deletarEndereco(eq(emptyMap));
     }
 
     @DisplayName("Deve retornar com erro EnderecoNaoExistenteException ao tentar remover um endereço")
     @Test
     public void testRemoverEnderecoError() throws Exception {
         String errorMessage = ControllersFixture.createErrorMessage("usuario você não pode limpar as informações do seu endereco porque ainda não adicionou um");
-        Map<String, String> errors = ConstructorErrors.returnMapErrors();
-        EnderecoNaoExistenteException enderecoNaoExistenteException = new EnderecoNaoExistenteException(errorMessage, Map.of("Endereço", "Endereço não cadastrado"));
+        errors.put("Endereço", "Endereço não cadastrado");
+        EnderecoNaoExistenteException enderecoNaoExistenteException = new EnderecoNaoExistenteException(errorMessage, errors);
 
-        when(usersService.deletarEndereco(eq(errors))).thenThrow(enderecoNaoExistenteException);
+        when(usersService.deletarEndereco(eq(emptyMap))).thenThrow(enderecoNaoExistenteException);
 
         mockMvc.perform(delete("/api/v1/users/enderecos/delete"))
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.code").value(HttpStatus.BAD_REQUEST.value()))
                 .andExpect(jsonPath("$.message").value(errorMessage))
                 .andExpect(jsonPath("$.fieldsErrors").isMap())
-                .andExpect(jsonPath("$.fieldsErrors['Endereço']").value("Endereço não cadastrado"));
+                .andExpect(jsonPath("$.fieldsErrors['Endereço']").value(errors.get("Endereço")));
 
-        verify(usersService, times(1)).deletarEndereco(errors);
+        verify(usersService, times(1)).deletarEndereco(eq(emptyMap));
     }
 
     @DisplayName("Deve atualizar com sucesso um endereço")
@@ -160,9 +166,8 @@ public class EnderecoControllerTest {
         EnderecoRequest enderecoRequest = ControllersFixture.createEnderecoRequest("Joao", "32", "nova", "São Paulo", "SP", "2233333");
         String enderecoRequestJson = objectMapper.writeValueAsString(enderecoRequest);
         EnderecoResponse enderecoResponse = ControllersFixture.createEnderecoResponse(9L, "Thiago Nunes", enderecoRequest.getRua(), enderecoRequest.getNumero(), enderecoRequest.getBairro(), enderecoRequest.getCidade(), enderecoRequest.getEstado(), enderecoRequest.getCep());
-        Map<String, String> errors = ConstructorErrors.returnMapErrors();
 
-        when(usersService.atualizarEndereco(eq(enderecoRequest), eq(errors))).thenReturn(enderecoResponse);
+        when(usersService.atualizarEndereco(eq(enderecoRequest), eq(emptyMap))).thenReturn(enderecoResponse);
 
         mockMvc.perform(put("/api/v1/users/enderecos/update")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -177,7 +182,7 @@ public class EnderecoControllerTest {
                 .andExpect(jsonPath("$.estado").value(enderecoResponse.getEstado()))
                 .andExpect(jsonPath("$.estado").value(enderecoResponse.getEstado()));
 
-        verify(usersService, times(1)).atualizarEndereco(enderecoRequest, errors);
+        verify(usersService, times(1)).atualizarEndereco(eq(enderecoRequest), eq(emptyMap));
     }
 
     @DisplayName("Deve retornar com erro AtualizarEnderecoException ao tentar atualizar um endereço")
@@ -185,12 +190,14 @@ public class EnderecoControllerTest {
     public void testAtualizarEnderecoError() throws Exception {
         EnderecoRequest enderecoRequest = ControllersFixture.createEnderecoRequest("Manu", "50", "Santo Antonio", "Rio de Janeiro", "Estado errado", "13123");
         String enderecoRequestJson = objectMapper.writeValueAsString(enderecoRequest);
-        Map<String, String> errors = ConstructorErrors.returnMapErrors();
+        errors.put("Endereço", "Endereço não cadastrado");
+        errors.put("Estado", "Sigla de estado inválido");
+
         String errorMessage = ControllersFixture.createErrorMessage("usuario, houve um erro na atualização do seu endereço");
-        AtualizarEnderecoException atualizarEnderecoException = new AtualizarEnderecoException(errorMessage, Map.of("Endereço", "Endereço não cadastrado", "Estado", "Sigla de estado inválido"));
+        AtualizarEnderecoException atualizarEnderecoException = new AtualizarEnderecoException(errorMessage, errors);
 
 
-        when(usersService.atualizarEndereco(eq(enderecoRequest), eq(errors))).thenThrow(atualizarEnderecoException);
+        when(usersService.atualizarEndereco(eq(enderecoRequest), eq(emptyMap))).thenThrow(atualizarEnderecoException);
 
         mockMvc.perform(put("/api/v1/users/enderecos/update")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -199,9 +206,9 @@ public class EnderecoControllerTest {
                 .andExpect(jsonPath("$.code").value(HttpStatus.BAD_REQUEST.value()))
                 .andExpect(jsonPath("$.message").value(errorMessage))
                 .andExpect(jsonPath("$.fieldsErrors").isMap())
-                .andExpect(jsonPath("$.fieldsErrors['Endereço']").value("Endereço não cadastrado"))
-                .andExpect(jsonPath("$.fieldsErrors['Estado']").value("Sigla de estado inválido"));
+                .andExpect(jsonPath("$.fieldsErrors['Endereço']").value(errors.get("Endereço")))
+                .andExpect(jsonPath("$.fieldsErrors['Estado']").value(errors.get("Estado")));
 
-        verify(usersService, times(1)).atualizarEndereco(enderecoRequest, errors);
+        verify(usersService, times(1)).atualizarEndereco(eq(enderecoRequest), eq(emptyMap));
     }
 }
