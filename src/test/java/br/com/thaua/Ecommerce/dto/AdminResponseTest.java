f246794ad4924505dfd97a83a61c1202551518f8
Thauã Gabriel Sousa e Silva <tagashi.dev@gmail.com>
2025-05-04 12:25:16 -0300

    test: adicionar testes unitarios para dto AdminResponse

---
+++ src/test/java/br/com/thaua/Ecommerce/dto/AdminResponseTest.java
@@ -0,0 +1,29 @@
+package br.com.thaua.Ecommerce.dto;
+
+import br.com.thaua.Ecommerce.controllers.ControllersFixture;
+import br.com.thaua.Ecommerce.dto.admin.AdminResponse;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class AdminResponseTest {
+
+    @Test
+    public void testAdminResponse() {
+        Long id = 1L;
+        String name = "jonas";
+        String email = "jonas@gmail.com";
+        int contasBanidas = 5;
+        LocalDateTime ultimoAcesso = LocalDateTime.now();
+
+        AdminResponse adminResponse = ControllersFixture.createAdminResponse(id, name, email, contasBanidas, ultimoAcesso);
+
+        assertThat(adminResponse.getId()).isEqualTo(id);
+        assertThat(adminResponse.getName()).isEqualTo(name);
+        assertThat(adminResponse.getEmail()).isEqualTo(email);
+        assertThat(adminResponse.getContasBanidas()).isEqualTo(contasBanidas);
+        assertThat(adminResponse.getUltimoAcesso()).isEqualTo(ultimoAcesso);
+    }
+}
