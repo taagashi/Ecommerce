8740c6da8fb56cb69b38ee94b452409049248096
Thauã Gabriel Sousa e Silva <tagashi.dev@gmail.com>
2025-05-04 12:46:23 -0300

    test: adicionar testes unitarios para dto CategoriaResponse

---
+++ src/test/java/br/com/thaua/Ecommerce/dto/categoria/CategoriaResponseTest.java
@@ -0,0 +1,22 @@
+package br.com.thaua.Ecommerce.dto.categoria;
+
+import br.com.thaua.Ecommerce.controllers.ControllersFixture;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class CategoriaResponseTest {
+    @Test
+    public void testCategoriaResponse() {
+        Long id = 2L;
+        String nome = "panos";
+        String descricao = "categoria para panos";
+        int produtosAssociados = 3;
+        CategoriaResponse categoriaResponse = ControllersFixture.createCategoriaResponse(id, nome, descricao, produtosAssociados);
+
+        assertThat(categoriaResponse.getId()).isEqualTo(id);
+        assertThat(categoriaResponse.getNome()).isEqualTo(nome);
+        assertThat(categoriaResponse.getDescricao()).isEqualTo(descricao);
+        assertThat(categoriaResponse.getProdutosAssociados()).isEqualTo(produtosAssociados);
+    }
+}
